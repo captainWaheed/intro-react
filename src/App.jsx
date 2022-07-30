2e7731a6d5e358719501e5b75bfa2e0742da74d4
Abdul Waheed <thecaptain0078@gmail.com>
2022-07-30 19:06:24 +0500

    change props for students rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
 import React from "react";
 import Table from "./Table";
 import List from "./List";
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       buttonClicked: "",
       assignments: [],
       /*Below this line, add the students state variable*/ students: [],
       grades: {},
     };
-
     this.handleButtonClicked = this.handleButtonClicked.bind(this);
     this.addAssignment = this.addAssignment.bind(this);
     /*Uncomment the line below to bind the method*/
     this.addStudent = this.addStudent.bind(this);
     this.addGrade = this.addGrade.bind(this);
   }
-
   handleButtonClicked(buttonName) {
     this.setState({
       buttonClicked: buttonName,
     });
   }
-
   /*Check out this addAssignment method*/
   addAssignment(assignmentName) {
     this.setState({
       assignments: this.state.assignments.concat(assignmentName),
     });
   }
-
   /*Write an addStudent method here*/
   addStudent(studentName) {
     this.setState({
       students: this.state.students.concat(studentName),
     });
   }
-
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
     let assignmentName = assignment;
@@ -50,10 +43,8 @@ class App extends React.Component {
     grades[assignmentName][studentName] = score;
     this.setState({ grades: grades });
   }
-
   render() {
     let tabChoice = <div />;
-
     /*Uncomment below to render assignments*/
     if (this.state.buttonClicked === "assignments") {
       tabChoice = (
@@ -67,18 +58,16 @@ class App extends React.Component {
     }
 
     /* Change below to render students*/
-
     if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
-          placeholder="Add Students.."
+          placeholder="Add Students..."
           currList={this.state.students}
           addFunction={this.addStudent}
           title="Student Roster"
         />
       );
     }
-
     /* Uncomment lines below to render grades*/
     /*if (this.state.buttonClicked === "grades") {
       tabChoice = (
@@ -90,7 +79,6 @@ class App extends React.Component {
         />
       );
     }*/
-
     return (
       <div>
         <div className="Box Box--spacious f4">
